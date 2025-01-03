669309557dd0c0bd7dd846d787aac8283ef1b76d
JAYPARK <114995540+JAYPARKJ@users.noreply.github.com>
2025-01-04 04:53:58 +0900

    Create  Customer.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
-import { Component } from 'react';
+import Customer from './Component/Customer'; // 확장자 .js는 생략 가능
 
-// 웹사이트 화면 출력
-class App extends Component{
-  render() {
-  return (
-    <div className="gray-background">
-      <img src="logo" alt="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers =  [
+  {
+  'id' : 1,
+  'image' : 'https://picsum.photos/350/400',
+  'name' : '홍길동',
+  'birthday' : '950503',
+  'gender' : '남자',
+  'job' : '대학생'
+
+},
+{
+  'id' : 2,
+  'image' : 'https://picsum.photos/380/490',
+  'name' : '지원숭',
+  'birthday' : '960808',
+  'gender' : '남자',
+  'job' : '프로그래머'
+
+},
+{
+  'id' : 3,
+  'image' : 'https://picsum.photos/360/470',
+  'name' : '김메로',
+  'birthday' : '970809',
+  'gender' : '남자',
+  'job' : '디제이'
+
 }
+] 
+ 
+ 
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map( c => {
+            return (
+              <Customer 
+                // key 값 설정해줘야함
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
