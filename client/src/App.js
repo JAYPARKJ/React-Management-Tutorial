f3d46eadf3d68ef12cfd6c6b7851883fd4e5c9a6
JAYPARKJ <lewis08@naver.com>
2025-01-08 00:13:29 +0900

    Add get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import './App.css';
 import Customer from './Component/Customer'; // 확장자 .js는 생략 가능
 import { Table, TableBody, TableHead, TableRow, TableCell, Paper } from "@mui/material";
@@ -14,39 +14,29 @@ const Styles = {
     minWidth: 1080
   }
 };
-
-const customers = [
-  {
-    id: 1,
-    image: 'https://picsum.photos/350/400',
-    name: '홍길동',
-    birthday: '950503',
-    gender: '남자',
-    job: '대학생'
-  },
-  {
-    id: 2,
-    image: 'https://picsum.photos/380/490',
-    name: '지원숭',
-    birthday: '960808',
-    gender: '남자',
-    job: '프로그래머'
-  },
-  {
-    id: 3,
-    image: 'https://picsum.photos/360/470',
-    name: '김메로',
-    birthday: '970809',
-    gender: '남자',
-    job: '디제이'
-  }
-];
-
+ 
+ 
 const StyledPaper = styled(Paper)(Styles.root);  // Paper에 스타일을 적용한 컴포넌트
 const StyledTable = styled(Table)(Styles.table);  // Table에 스타일을 적용한 컴포넌트
 
 class App extends React.Component {
+
+  state = {
+    customers: ""
+  }
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+  
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
   render() {
+    const {classes} = this.props; 
     return (
       <StyledPaper>
         <StyledTable>
@@ -61,7 +51,7 @@ class App extends React.Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => {
+            {this.state.customers? this.state.customers.map(c => {
               return (
                 <Customer
                   key={c.id}
@@ -73,7 +63,7 @@ class App extends React.Component {
                   job={c.job}
                 />
               );
-            })}
+            }) : ""}
           </TableBody>
         </StyledTable>
       </StyledPaper>
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// 서버 모듈 기능 선언
+// 서버 모듈 기능 선언 express 기본 설정
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,12 +6,43 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+///////////////////////////////////////////////
 
 // 클라이언트가 api 경로에 hello 접속시 메세지 출력 처리 
-app.get('/api/hello', (req, res) => {
-    res.send({message : 'Hello Express!'});
-});
+//app.get('/api/hello', (req, res) => {
+//    res.send({message : 'Hello Express!'});
+//});
 
+// 이전 시간에 클라이언트에 명시해놨던 3명의 고객정보 데이터(json) 반환하기 
+app.get('/api/customers', (req, res) => {
+     res.send([
+        {
+          id: 1,
+          image: 'https://picsum.photos/350/400',
+          name: '홍길동',
+          birthday: '950503',
+          gender: '남자',
+          job: '대학생'
+        },
+        {
+          id: 2,
+          image: 'https://picsum.photos/380/490',
+          name: '지원숭',
+          birthday: '960808',
+          gender: '남자',
+          job: '프로그래머'
+        },
+        {
+          id: 3,
+          image: 'https://picsum.photos/360/470',
+          name: '김메로',
+          birthday: '970809',
+          gender: '남자',
+          job: '디제이'
+        }
+      ]);
+    
+});
 // 5000번 포트로 동작 시킴, 서버 동작 중 출력 메세지
 
 app.listen(port, () => console.log('Listening on port ${port}'));
